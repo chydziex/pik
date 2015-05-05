cb9a9e935101ae1f8ce8254fba815885ee0b52e8
chydziex <mateusz.chydzinski@onet.eu>
2015-05-05 15:42:09 +0200

    usunalem niepotrzebny folder

--- 1/src/main/java/app/SpringContextConfiguration.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
- 
-@Configuration
-@ComponentScan("app")
-public class SpringContextConfiguration {
-}
